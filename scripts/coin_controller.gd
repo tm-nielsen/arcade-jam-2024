0bf145f6595a093b8cbac71505d7e82c6b7a0772
Twig Nielsen <littleb88888888@gmail.com>
2024-06-01 01:56:18 -0600

    Improved coin - added coin sprite - made coin randomly rotated on spawn

--- scripts/coin_controller.gd
+++ scripts/coin_controller.gd
@@ -5,6 +5,7 @@ extends RigidBody2D
 func _ready():
   linear_damp = 0
   body_entered.connect(_on_body_entered)
+  rotation = randf() * 360
 
 func _on_body_entered(_body: PhysicsBody2D):
   linear_damp = post_collision_damping
