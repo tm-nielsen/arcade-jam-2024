ed727ad05a17f1b434b5ab1f1d6d84e82e618948
Twig Nielsen <littleb88888888@gmail.com>
2024-06-01 16:31:53 -0600

    Added safety margin to enemy spawner

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -2,6 +2,7 @@ class_name EnemySpawner
 extends Node2D
 
 @export var spawn_area := Vector2(400, 200)
+@export var player_safety_margin : float = 80
 
 @export_subgroup("references")
 @export var bird_prefab: PackedScene
@@ -26,6 +27,18 @@ func spawn_enemy():
   new_enemy.position = _get_spawn_position()
 
 func _get_spawn_position() -> Vector2:
+  var random_spawn_position = _get_random_spawn_position()
+  while _is_point_too_close_to_player(random_spawn_position):
+    random_spawn_position = _get_random_spawn_position()
+  return random_spawn_position
+
+func _is_point_too_close_to_player(point: Vector2) -> bool:
+  for player in get_tree().get_nodes_in_group("players"):
+    if point.distance_to(player.position) < player_safety_margin:
+      return true
+  return false
+
+func _get_random_spawn_position() -> Vector2:
   var spawn_position = Vector2()
   spawn_position.x = _get_random_point_in_span(spawn_area.x)
   spawn_position.y = _get_random_point_in_span(spawn_area.y)
