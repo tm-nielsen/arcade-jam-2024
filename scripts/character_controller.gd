629eaacd86d3cd1d1843c3f8d61faf158fe84bc1
Twig Nielsen <littleb88888888@gmail.com>
2024-05-31 20:00:30 -0600

    Added scene setup - added arcade machine input map - added window settings -added start of character controller

---
+++ scripts/character_controller.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+
+@export var acceleration: float = 40
+@export var movement_speed: float = 20
+@export var dash_speed: float = 40
+@export var dash_duration: float = 0.5
+
+
