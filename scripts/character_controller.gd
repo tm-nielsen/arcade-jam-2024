0e184ff8d3ea553bc993995e9dcb2b52e486d4bd
Twig Nielsen <littleb88888888@gmail.com>
2024-06-01 01:04:55 -0600

    Added basic coin throwing

--- scripts/character_controller.gd
+++ scripts/character_controller.gd
@@ -1,13 +1,19 @@
 extends CharacterBody2D
 
-enum PlayerState {NEUTRAL, DASHING, DAMAGED}
+enum PlayerState {NEUTRAL, DASHING, THROWING, DAMAGED}
 
 @export_range(1, 2) var player_number: int = 1
 @export var acceleration: float = 40
 @export var movement_speed: float = 200
+
+@export_subgroup("dash", "dash")
 @export var dash_initial_acceleration: float = 200
-@export var dash_speed: float = 600
+@export var dash_movement_speed: float = 600
 @export var dash_timer: Timer
+
+@export_subgroup("throwing")
+@export var coin_thrower: CoinThrower
+@export var aiming_movement_speed: float = 60
 
 var state: PlayerState
 
@@ -16,40 +22,62 @@ func _ready():
   if player_number > PlayerCountSelector.playerCount:
     queue_free()
   dash_timer.timeout.connect(_on_dash_timer_timeout)
+  coin_thrower.aiming_started.connect(_on_aiming_started)
+  coin_thrower.coin_thrown.connect(_on_coin_thrown)
 
 
 func _physics_process(_delta):
-  var input_direction := get_input_direction()
+  var input_direction := get_numbered_input_direction()
+  coin_thrower.process(input_direction, is_numbered_action_pressed("throw"))
 
   if input_direction != Vector2.ZERO:
     velocity += acceleration * input_direction
 
-    var maximum_speed = dash_speed if state == PlayerState.DASHING else movement_speed
+    var maximum_speed = get_maximum_speed()
     velocity = _clamp_vector_length(velocity, maximum_speed)
   else:
     velocity = Vector2.ZERO
 
   if is_numbered_action_just_pressed("dash") && state == PlayerState.NEUTRAL:
     state = PlayerState.DASHING
+    coin_thrower.disable()
     velocity += input_direction * dash_initial_acceleration
     dash_timer.start()
 
   move_and_slide()
 
 
-func _clamp_vector_length(v: Vector2, limit: float) -> Vector2:
-  if v.length() > limit:
-    return v.normalized() * limit
-  return v
+func get_maximum_speed() -> float:
+  if coin_thrower.is_aiming:
+    return aiming_movement_speed
+  if state == PlayerState.DASHING:
+    return dash_movement_speed
+  return movement_speed
 
 
-func get_input_direction() -> Vector2:
+func _on_aiming_started():
+  pass
+
+func _on_coin_thrown():
+  pass
+
+
+func _on_dash_timer_timeout():
+  if state == PlayerState.DASHING:
+    state = PlayerState.NEUTRAL
+    coin_thrower.enable()
+
+
+func get_numbered_input_direction() -> Vector2:
   var vertical_input_direction = _get_numbered_input_axis("up", "down")
   var horizontal_input_direction = _get_numbered_input_axis("left", "right")
   return Vector2(horizontal_input_direction, vertical_input_direction).normalized()
 
 func is_numbered_action_just_pressed(simple_name: String) -> bool:
   return Input.is_action_just_pressed(_get_input_name(simple_name))
+
+func is_numbered_action_pressed(simple_name: String) -> bool:
+  return Input.is_action_pressed(_get_input_name(simple_name))
 
 func _get_numbered_input_axis(negative_action: String, positive_action: String) -> float:
   var negative_action_name = _get_input_name(negative_action)
@@ -59,7 +87,8 @@ func _get_numbered_input_axis(negative_action: String, positive_action: String) 
 func _get_input_name(simple_name: String) -> String:
   return ("p%d_" % player_number) + simple_name
 
-
-func _on_dash_timer_timeout():
-  if state == PlayerState.DASHING:
-    state = PlayerState.NEUTRAL
+    
+func _clamp_vector_length(v: Vector2, limit: float) -> Vector2:
+  if v.length() > limit:
+    return v.normalized() * limit
+  return v
---
+++ scripts/coin_thrower.gd
@@ -0,0 +1,45 @@
+class_name CoinThrower
+extends Node2D
+
+signal aiming_started
+signal coin_thrown
+
+enum ThrowerState {CAN_THROW, AIMING, DISABLED}
+
+@export var coin_prefab: PackedScene
+@export var throw_speed: float = 800
+@export var throw_offset: float = 20
+
+var is_aiming: get = get_is_aiming
+var state: ThrowerState
+
+
+func process(input_direction: Vector2, is_throw_pressed: bool):
+  if is_throw_pressed:
+    if state == ThrowerState.CAN_THROW:
+      state = ThrowerState.AIMING
+      aiming_started.emit()
+
+  elif state == ThrowerState.AIMING && input_direction:
+    throw_coin(input_direction)
+
+func throw_coin(direction: Vector2):
+  var new_coin: RigidBody2D = coin_prefab.instantiate()
+  add_child(new_coin)
+
+  new_coin.top_level = true
+  new_coin.position = global_position + direction * throw_offset;
+  new_coin.linear_velocity = direction * throw_speed
+
+  state = ThrowerState.CAN_THROW
+  coin_thrown.emit()
+
+
+func get_is_aiming() -> bool:
+  return state == ThrowerState.AIMING
+
+func disable():
+  state = ThrowerState.DISABLED
+
+func enable():
+  state = ThrowerState.CAN_THROW
