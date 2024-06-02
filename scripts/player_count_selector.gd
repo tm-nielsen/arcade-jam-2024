069b243476c3eb8df4b1c1baffd886cf094c4c2e
Twig Nielsen <littleb88888888@gmail.com>
2024-06-01 18:42:39 -0600

    Fixed bug with player count selector

--- scripts/player_count_selector.gd
+++ scripts/player_count_selector.gd
@@ -11,9 +11,11 @@ func _process(_delta):
     if playerCount == 1 && Input.is_action_just_pressed("select_2_players"):
       playerCount = 2
       get_tree().reload_current_scene()
+      ScoreManager.current_score = 0
     if playerCount == 2 && Input.is_action_just_pressed("select_1_player"):
       playerCount = 1
       get_tree().reload_current_scene()
+      ScoreManager.current_score = 0
 
   else:
     if (Input.is_action_just_pressed("select_1_player") && playerCount != 1) || \
