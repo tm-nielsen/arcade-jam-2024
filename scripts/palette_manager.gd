9e02622bcdbc9a881ea14cd13e959ed2277e45a8
Twig Nielsen <littleb88888888@gmail.com>
2024-06-02 01:54:43 -0600

    Added more palettes - added more palettes to palette manager - implemented buttons to manually select a palette, overriding automatic switched

--- scripts/palette_manager.gd
+++ scripts/palette_manager.gd
@@ -9,12 +9,33 @@ extends Control
 var palette_threshold_index: int = 0
 var last_palette_threshold: int = 0
 
+var palette_override_active: bool
+var selected_palette_index = 0
+
 
 func _ready():
   tutorial_palette.apply()
   ScoreManager.score_increased.connect(_on_score_increased)
 
+func _process(_delta):
+  if Input.is_action_just_pressed("toggle_palette_up"):
+    _move_palette_selection(1)
+  elif Input.is_action_just_pressed("toggle_palette_down"):
+    _move_palette_selection(-1)
+
+func _move_palette_selection(increment: int):
+  if palette_override_active:
+    selected_palette_index = (selected_palette_index + increment) % palettes.size()
+    palettes[selected_palette_index].apply()
+  else:
+    palettes[0].apply()
+    palette_override_active = true
+
+
 func _on_score_increased(points_added: int):
+  if palette_override_active:
+    return
+    
   var current_score = ScoreManager.current_score
   if current_score == points_added:
     starting_palette.apply()
