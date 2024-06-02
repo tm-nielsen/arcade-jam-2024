ca5ca8d8ddbb8919e50c2f46935d71c3eba5d75e
Twig Nielsen <littleb88888888@gmail.com>
2024-06-01 21:11:40 -0600

    Fixed palette manager threshold logic

--- scripts/palette_manager.gd
+++ scripts/palette_manager.gd
@@ -4,7 +4,7 @@ extends Control
 @export var starting_palette: Palette
 @export var palettes: Array[Palette]
 
-@export var palette_thresholds: PackedInt32Array = [400, 600, 1000]
+@export var palette_thresholds: PackedInt32Array = [200, 300, 500, 1000]
 
 var palette_threshold_index: int = 0
 var last_palette_threshold: int = 0
@@ -23,6 +23,6 @@ func _on_score_increased(points_added: int):
     var distance_from_last_threshold = current_score - last_palette_threshold
     var next_pallette_threshold = palette_thresholds[palette_threshold_index]
     if distance_from_last_threshold > next_pallette_threshold:
-      last_palette_threshold = next_pallette_threshold
+      last_palette_threshold += next_pallette_threshold
       palette_threshold_index = clampi(palette_threshold_index + 1, 0, palette_thresholds.size() -1)
       palettes.pick_random().apply()
