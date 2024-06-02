4d65cef0848ae8b89dc7d78da06695bca5388221
Twig Nielsen <littleb88888888@gmail.com>
2024-06-02 09:17:01 -0600

    Prevented the repeated random selection of a palette

--- scripts/palette_manager.gd
+++ scripts/palette_manager.gd
@@ -9,6 +9,8 @@ extends Control
 @export_subgroup("audio")
 @export var audio_player: AudioStreamPlayer2D
 
+var active_palette: Palette
+
 var palette_threshold_index: int = 0
 var last_palette_threshold: int = 0
 
@@ -29,10 +31,11 @@ func _process(_delta):
 func _move_palette_selection(increment: int):
   if palette_override_active:
     selected_palette_index = (selected_palette_index + increment) % palettes.size()
-    palettes[selected_palette_index].apply()
+    active_palette = palettes[selected_palette_index]
   else:
-    palettes[0].apply()
+    active_palette = palettes[0]
     palette_override_active = true
+  active_palette.apply()
 
 
 func _on_score_increased(points_added: int):
@@ -49,5 +52,12 @@ func _on_score_increased(points_added: int):
     if distance_from_last_threshold > next_pallette_threshold:
       last_palette_threshold += next_pallette_threshold
       palette_threshold_index = clampi(palette_threshold_index + 1, 0, palette_thresholds.size() -1)
-      palettes.pick_random().apply()
       audio_player.play()
+      
+      var random_palette = palettes.pick_random()
+      if random_palette != active_palette:
+        active_palette = random_palette
+      else:
+        var incremented_index = palettes.find(random_palette) + 1
+        active_palette = palettes[incremented_index % palettes.size()]
+      active_palette.apply()
