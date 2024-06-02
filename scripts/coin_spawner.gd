9af494e6007e500c9b36ce82987b0700f1918a23
Twig Nielsen <littleb88888888@gmail.com>
2024-06-02 09:28:06 -0600

    Augmented coin spawning - made coins spawn with a slight offset if the last one spawned has not been touched

--- scripts/coin_spawner.gd
+++ scripts/coin_spawner.gd
@@ -3,10 +3,14 @@ extends Node2D
 @export var coin_pickup_prefab: PackedScene
 @export var coin_drop_thresholds: PackedInt32Array = [400, 1000, 1600, 2500, 4000, 8000, 20000]
 @export var two_player_spawn_offset: float = 64
+@export var extant_coin_spawn_offset: float = 16
 @export var empty_bank_mercy_delay: float = 2
 
 var next_threshold_index: int = 0
 var mercy_tween: Tween
+
+var last_spawned_coins: Array[CoinController]
+var last_spawned_coin_y_pos: float
 
 
 func _ready():
@@ -20,19 +24,35 @@ func _process(_delta):
 
 
 func spawn_coin():
+    if _is_last_spawned_coin_untouched():
+      last_spawned_coin_y_pos += extant_coin_spawn_offset
+    else:
+      last_spawned_coin_y_pos = 0
+
     var coin_pickup = coin_pickup_prefab.instantiate()
+    last_spawned_coins = [coin_pickup]
     add_child.call_deferred(coin_pickup)
+    coin_pickup.position.y = last_spawned_coin_y_pos
 
     if PlayerCountSelector.playerCount == 2:
       coin_pickup.position.x = two_player_spawn_offset
 
       coin_pickup = coin_pickup_prefab.instantiate()
+      last_spawned_coins.append(coin_pickup)
       add_child.call_deferred(coin_pickup)
       coin_pickup.position.x = -two_player_spawn_offset
+      coin_pickup.position.y = last_spawned_coin_y_pos
 
 func _players_have_coins() -> bool:
   for player in get_tree().get_nodes_in_group("players"):
     if player.coins_held > 0:
+      return true
+  return false
+
+
+func _is_last_spawned_coin_untouched() -> bool:
+  for coin in last_spawned_coins:
+    if coin && coin.position.y == last_spawned_coin_y_pos:
       return true
   return false
 
