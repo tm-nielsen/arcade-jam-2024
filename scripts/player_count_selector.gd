1eae326a4ad384bc8e08b0a8311929f7d3c17037
Twig Nielsen <littleb88888888@gmail.com>
2024-06-01 20:44:43 -0600

    Fixed palette swapping with two players - preserved primary colour in highlight elements - mainlined player one and two specific colours

--- scripts/player_count_selector.gd
+++ scripts/player_count_selector.gd
@@ -9,19 +9,21 @@ func _ready():
 func _process(_delta):
   if visible:
     if playerCount == 1 && Input.is_action_just_pressed("select_2_players"):
-      playerCount = 2
-      get_tree().reload_current_scene()
-      ScoreManager.current_score = 0
+      _set_player_count(2)
     if playerCount == 2 && Input.is_action_just_pressed("select_1_player"):
-      playerCount = 1
-      get_tree().reload_current_scene()
-      ScoreManager.current_score = 0
+      _set_player_count(1)
 
   else:
     if (Input.is_action_just_pressed("select_1_player") && playerCount != 1) || \
       (Input.is_action_just_pressed("select_2_players") && playerCount != 2):
       visible = true
 
+
+func _set_player_count(new_player_count: int):
+  playerCount = new_player_count
+  RenderingServer.global_shader_parameter_set("player_count", new_player_count)
+  get_tree().reload_current_scene()
+  ScoreManager.current_score = 0
 
 func _input(event):
   if event.is_action("select_1_player") && playerCount != 1 || \
